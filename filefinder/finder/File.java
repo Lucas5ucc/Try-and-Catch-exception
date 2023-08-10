02b15cb1989801f1df078d529931f8b6f02203c2
Lucas Rocha <139708739+Lucas5ucc@users.noreply.github.com>
2023-08-10 10:43:47 +0100

    Add files via upload

---
+++ filefinder/finder/File.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.finder;
+public class File {
+
+    String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
