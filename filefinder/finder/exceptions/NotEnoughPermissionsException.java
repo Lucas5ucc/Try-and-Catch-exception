b402b1741cd534c9ddda25e11fb9d9ccfdbd64fa
Lucas Rocha <139708739+Lucas5ucc@users.noreply.github.com>
2023-10-08 21:16:48 +0100

    Update NotEnoughPermissionsException.java
    clean

--- filefinder/finder/exceptions/NotEnoughPermissionsException.java
+++ filefinder/finder/exceptions/NotEnoughPermissionsException.java
@@ -1,8 +1,5 @@
 package org.academiadecodigo.finder.exceptions;
 
-/**
- * Created by pantoninho on 22/05/16.
- */
 public class NotEnoughPermissionsException extends FileException {
 
     public NotEnoughPermissionsException() {
