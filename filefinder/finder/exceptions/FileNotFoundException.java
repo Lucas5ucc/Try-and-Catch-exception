99ec1b841cb4db55541db15f4f016ed4a42f852f
Lucas Rocha <139708739+Lucas5ucc@users.noreply.github.com>
2023-10-08 21:16:33 +0100

    Update FileNotFoundException.java
    clean

--- filefinder/finder/exceptions/FileNotFoundException.java
+++ filefinder/finder/exceptions/FileNotFoundException.java
@@ -1,8 +1,6 @@
 package org.academiadecodigo.finder.exceptions;
 
-/**
- * Created by pantoninho on 22/05/16.
- */
+
 public class FileNotFoundException extends FileException {
 
     public FileNotFoundException() {
